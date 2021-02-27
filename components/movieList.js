ef3e1a22ab9516e11647d9ee22e04243baeacb90
PAWANANJANI KUMAR <pawananjanimth1@gmail.com>
2021-02-27 16:42:51 +0530

    Code splitted into smaller omponents

---
+++ components/movieList.js
@@ -0,0 +1,135 @@
+import React from 'react';
+
+
+class MovieList extends React.Component{
+
+// state = {
+//     count: 0
+// }
+
+   constructor(props){
+       super(props);
+       this.state={count:0};
+   }
+    increment = ()=>{
+        const {count} = this.state;
+        this.setState({count:count+1});
+    }
+
+    decrement = ()=>{
+        const {count} = this.state;
+        this.setState({count:count-1});
+    }
+
+
+render (){
+   return (<React.Fragment>
+          
+          <div>
+            <button className="btn btn-primary" onClick={this.increment}>Increment Number</button>
+            <button className="btn btn-primary" onClick={this.decrement}>Decrement Number</button>
+            <h1>{this.state.count}</h1>
+          </div>
+        <div className="col-lg-4 col-md-6 mb-4">
+            <div className="card h-100">
+              <a href="#"><img className="card-img-top" src="http://placehold.it/700x400" alt="" /></a>
+              <div className="card-body">
+                <h4 className="card-title">
+                  <a href="#">Item One</a>
+                </h4>
+                <h5>$24.99</h5>
+                <p className="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet numquam aspernatur!</p>
+              </div>
+              <div className="card-footer">
+                <small className="text-muted">&#9733; &#9733; &#9733; &#9733; &#9734;</small>
+              </div>
+            </div>
+          </div>
+
+          <div className="col-lg-4 col-md-6 mb-4">
+            <div className="card h-100">
+              <a href="#"><img className="card-img-top" src="http://placehold.it/700x400" alt="" /></a>
+              <div className="card-body">
+                <h4 className="card-title">
+                  <a href="#">Item Two</a>
+                </h4>
+                <h5>$24.99</h5>
+                <p className="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet numquam aspernatur! Lorem ipsum dolor sit amet.</p>
+              </div>
+              <div className="card-footer">
+                <small className="text-muted">&#9733; &#9733; &#9733; &#9733; &#9734;</small>
+              </div>
+            </div>
+          </div>
+
+          <div className="col-lg-4 col-md-6 mb-4">
+            <div className="card h-100">
+              <a href="#"><img className="card-img-top" src="http://placehold.it/700x400" alt="" /></a>
+              <div className="card-body">
+                <h4 className="card-title">
+                  <a href="#">Item Three</a>
+                </h4>
+                <h5>$24.99</h5>
+                <p className="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet numquam aspernatur!</p>
+              </div>
+              <div className="card-footer">
+                <small className="text-muted">&#9733; &#9733; &#9733; &#9733; &#9734;</small>
+              </div>
+            </div>
+          </div>
+
+          <div className="col-lg-4 col-md-6 mb-4">
+            <div className="card h-100">
+              <a href="#"><img className="card-img-top" src="http://placehold.it/700x400" alt="" /></a>
+              <div className="card-body">
+                <h4 className="card-title">
+                  <a href="#">Item Four</a>
+                </h4>
+                <h5>$24.99</h5>
+                <p className="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet numquam aspernatur!</p>
+              </div>
+              <div className="card-footer">
+                <small className="text-muted">&#9733; &#9733; &#9733; &#9733; &#9734;</small>
+              </div>
+            </div>
+          </div>
+
+          <div className="col-lg-4 col-md-6 mb-4">
+            <div className="card h-100">
+              <a href="#"><img className="card-img-top" src="http://placehold.it/700x400" alt="" /></a>
+              <div className="card-body">
+                <h4 className="card-title">
+                  <a href="#">Item Five</a>
+                </h4>
+                <h5>$24.99</h5>
+                <p className="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet numquam aspernatur! Lorem ipsum dolor sit amet.</p>
+              </div>
+              <div className="card-footer">
+                <small className="text-muted">&#9733; &#9733; &#9733; &#9733; &#9734;</small>
+              </div>
+            </div>
+          </div>
+
+          <div className="col-lg-4 col-md-6 mb-4">
+            <div className="card h-100">
+              <a href="#"><img className="card-img-top" src="http://placehold.it/700x400" alt="" /></a>
+              <div className="card-body">
+                <h4 className="card-title">
+                  <a href="#">Item Six</a>
+                </h4>
+                <h5>$24.99</h5>
+                <p className="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet numquam aspernatur!</p>
+              </div>
+              <div className="card-footer">
+                <small className="text-muted">&#9733; &#9733; &#9733; &#9733; &#9734;</small>
+              </div>
+            </div>
+          </div>
+
+    </React.Fragment>);
+}
+   
+};
+
+
+export default MovieList;
--- pages/about.js
+++ pages/about.js
@@ -1,11 +1,32 @@
+import React from 'react';
 
 
-const About = ()=>{
-    const message = "Variable";
+//Functional components:
+//WHEN TO USE:
+//1.Smaller components
+//2.Reusable components
+//Presentational components - partially right, we can use hooks and specify state
 
-    return (
-        <div>About Page-{message}</div>
-    );
+// const About = ()=>{
+//     const message = "Variable";
+
+//     return (
+//         <div>About Page-{message}</div>
+//     );
+// }
+
+class About extends React.Component{
+   
+    
+    render (){
+        const message = "Class";
+        return (
+            <div>
+              <h1>I am a {message} component.</h1>
+            </div>
+           
+        );
+    }
 }
 
 
