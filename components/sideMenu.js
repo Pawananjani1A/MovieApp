a71937c99ab5fab9c5b75dedc2d0c4ce9a242cd8
PAWANANJANI KUMAR <pawananjanimth1@gmail.com>
2021-02-27 18:28:55 +0530

    State in functional and clas components

--- components/sideMenu.js
+++ components/sideMenu.js
@@ -1,9 +1,16 @@
-
+import {useState} from 'react';
 
 
 const SideMenu = ()=>{
 
+  const [count,setCount] = useState(0);
+   const increment = ()=>{
+        setCount(count+1);
+    }
 
+   const decrement = ()=>{
+        setCount(count-1);
+    }
 
     return  (
         <div>
@@ -13,7 +20,11 @@ const SideMenu = ()=>{
             <a href="#" className="list-group-item">Category 2</a>
             <a href="#" className="list-group-item">Category 3</a>
             </div>
-
+           <div>
+            <button className="btn btn-primary" onClick={increment}>Increment Number</button>
+            <button className="btn btn-primary" onClick={decrement}>Decrement Number</button>
+            <h1>{count}</h1>
+          </div>
         </div>
     );
 };
