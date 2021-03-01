fa3fd9df4ed8b4b270576db25204e0c8787933d7
PAWANANJANI KUMAR <pawananjanimth1@gmail.com>
2021-03-01 19:50:46 +0530

    Fixing navlink highlighting

--- components/navbar.js
+++ components/navbar.js
@@ -1,7 +1,12 @@
 import Link from "next/link";
+import { useRouter } from 'next/router';
 
 const Navbar = ()=>{
 
+  const router = useRouter();
+  const {pathname} = router;
+
+  // console.log(router);
 
 
     return ( <nav className="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
@@ -12,24 +17,24 @@ const Navbar = ()=>{
                       </button>
                       <div className="collapse navbar-collapse" id="navbarResponsive">
                         <ul className="navbar-nav ml-auto">
-                          <li className="nav-item active">
+                          <li className={`nav-item ${pathname==='/'?'active':''}`}>
                           <Link href="/">
                               <a className="nav-link" >Home
                               <span className="sr-only">(current)</span>
                             </a>
                           </Link>
                           </li>
-                          <li className="nav-item">
+                          <li className={`nav-item ${pathname==='/about'?'active':''}`}>
                           <Link href="/about">
                             <a className="nav-link" >About</a>
                             </Link>
                           </li>
-                          <li className="nav-item">
+                          <li className={`nav-item ${pathname==='/services'?'active':''}`}>
                           <Link href="/services">
                             <a className="nav-link">Services</a>
                             </Link>
                           </li>
-                          <li className="nav-item">
+                          <li className={`nav-item ${pathname==='/contact'?'active':''}`}>
                           <Link href="/contact">
                             <a className="nav-link" >Contact</a>
                             </Link>
